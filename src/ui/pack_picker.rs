b004b581752087dff7da2742e18a458977303df6
panicbit <panicbit.dev@gmail.com>
2024-10-14 04:39:28 +0200

    refactor pack picker

--- src/ui/pack_picker.rs
+++ src/ui/pack_picker.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::os::windows::fs::FileTypeExt;
 use std::path::{Path, PathBuf};
 
-use egui::{CentralPanel, ScrollArea, SidePanel, Ui, Widget};
+use egui::{CentralPanel, CollapsingHeader, Grid, ScrollArea, SidePanel, Ui, Widget, WidgetText};
 use eyre::{Error, Result};
 
 use crate::pack::{manifest, Manifest, Pack};
@@ -10,7 +10,6 @@ use crate::pack::{manifest, Manifest, Pack};
 pub struct PackPicker {
     packs_path: PathBuf,
     pack_manifests: Option<Vec<(PathBuf, Manifest)>>,
-    selected_manifest: usize,
     error: Option<Error>,
 }
 
@@ -19,13 +18,23 @@ impl PackPicker {
         Self {
             packs_path: packs_path.into(),
             pack_manifests: None,
-            selected_manifest: 0,
             error: None,
         }
     }
 
     pub fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) -> Option<Pack> {
-        let mut pack = None;
+        let mut loaded_pack = None;
+
+        CentralPanel::default().show(ctx, |ui| {
+            let pack_manifests = self.get_or_load_manifests();
+
+            show_packs(ui, pack_manifests, &mut loaded_pack)
+        });
+
+        loaded_pack
+    }
+
+    fn get_or_load_manifests(&mut self) -> &[(PathBuf, Manifest)] {
         let pack_manifests = &*self.pack_manifests.get_or_insert_with(|| {
             match try_read_pack_manifests(&self.packs_path) {
                 Ok(pack_manifests) => pack_manifests,
@@ -35,44 +44,50 @@ impl PackPicker {
                 }
             }
         });
-
-        SidePanel::right("pack_picker_right_side").show(ctx, |ui| {
-            ScrollArea::vertical().show(ui, |ui| {
-                let Some((manifest_path, manifest)) = pack_manifests.get(self.selected_manifest)
-                else {
-                    return;
-                };
-
-                ui.vertical(|ui| {
-                    for (variant_id, variant) in &manifest.variants {
-                        if ui.button(&variant.display_name).clicked() {
-                            match Pack::load(manifest_path, variant_id) {
-                                Ok(loaded_pack) => {
-                                    pack = Some(loaded_pack);
-                                    ctx.request_repaint();
-                                }
-                                Err(err) => eprintln!("{err:#?}"),
-                            }
-                        }
-                    }
-                });
-
-                ui.allocate_space(ui.available_size());
-            });
-        });
-
-        CentralPanel::default().show(ctx, |ui| {
-            ui.vertical(|ui| {
-                for (manifest_index, (_, manifest)) in pack_manifests.iter().enumerate() {
-                    if ui.button(&manifest.name).clicked() {
-                        self.selected_manifest = manifest_index;
-                    }
-                }
-            });
-        });
-
-        pack
+        pack_manifests
     }
+}
+
+fn show_packs(ui: &mut Ui, pack_manifests: &[(PathBuf, Manifest)], loaded_pack: &mut Option<Pack>) {
+    ScrollArea::vertical().show(ui, |ui| {
+        ui.vertical(|ui| {
+            for (manifest_path, manifest) in pack_manifests {
+                let pack_name = WidgetText::from(&manifest.name).heading();
+
+                CollapsingHeader::new(pack_name)
+                    .default_open(false)
+                    .show(ui, |ui| {
+                        show_variants(ui, manifest, manifest_path, loaded_pack);
+                    });
+
+                ui.end_row();
+            }
+        });
+
+        ui.allocate_space(ui.available_size());
+    });
+}
+
+fn show_variants(
+    ui: &mut Ui,
+    manifest: &Manifest,
+    manifest_path: &PathBuf,
+    loaded_pack: &mut Option<Pack>,
+) {
+    ui.horizontal(|ui| {
+        for (variant_id, variant) in &manifest.variants {
+            if ui.button(&variant.display_name).clicked() {
+                match Pack::load(manifest_path, variant_id) {
+                    Ok(pack) => {
+                        *loaded_pack = Some(pack);
+                    }
+                    Err(err) => eprintln!("{err:#?}"),
+                }
+            }
+
+            ui.end_row();
+        }
+    });
 }
 
 fn try_read_pack_manifests(packs_path: &Path) -> Result<Vec<(PathBuf, Manifest)>> {
