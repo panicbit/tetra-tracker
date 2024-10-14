98c6ece52e5b4211c80d99c91afc29107314bf6f
panicbit <panicbit.dev@gmail.com>
2024-10-14 02:00:01 +0200

    better pack picker ui

--- src/ui/pack_picker.rs
+++ src/ui/pack_picker.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::os::windows::fs::FileTypeExt;
 use std::path::{Path, PathBuf};
 
-use egui::{CentralPanel, Ui, Widget};
+use egui::{CentralPanel, ScrollArea, SidePanel, Ui, Widget};
 use eyre::{Error, Result};
 
 use crate::pack::{manifest, Manifest, Pack};
@@ -26,50 +26,48 @@ impl PackPicker {
 
     pub fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) -> Option<Pack> {
         let mut pack = None;
+        let pack_manifests = &*self.pack_manifests.get_or_insert_with(|| {
+            match try_read_pack_manifests(&self.packs_path) {
+                Ok(pack_manifests) => pack_manifests,
+                Err(err) => {
+                    self.error = Some(err);
+                    Vec::new()
+                }
+            }
+        });
+
+        SidePanel::right("pack_picker_right_side").show(ctx, |ui| {
+            ScrollArea::vertical().show(ui, |ui| {
+                let Some((manifest_path, manifest)) = pack_manifests.get(self.selected_manifest)
+                else {
+                    return;
+                };
+
+                ui.vertical(|ui| {
+                    for (variant_id, variant) in &manifest.variants {
+                        if ui.button(&variant.display_name).clicked() {
+                            match Pack::load(manifest_path, variant_id) {
+                                Ok(loaded_pack) => {
+                                    pack = Some(loaded_pack);
+                                    ctx.request_repaint();
+                                }
+                                Err(err) => eprintln!("{err:#?}"),
+                            }
+                        }
+                    }
+                });
+
+                ui.allocate_space(ui.available_size());
+            });
+        });
 
         CentralPanel::default().show(ctx, |ui| {
-            let pack_manifests = &*self.pack_manifests.get_or_insert_with(|| {
-                match try_read_pack_manifests(&self.packs_path) {
-                    Ok(pack_manifests) => pack_manifests,
-                    Err(err) => {
-                        self.error = Some(err);
-                        Vec::new()
+            ui.vertical(|ui| {
+                for (manifest_index, (_, manifest)) in pack_manifests.iter().enumerate() {
+                    if ui.button(&manifest.name).clicked() {
+                        self.selected_manifest = manifest_index;
                     }
                 }
-            });
-
-            ui.horizontal(|ui| {
-                ui.group(|ui| {
-                    ui.vertical(|ui| {
-                        for (manifest_index, (_, manifest)) in pack_manifests.iter().enumerate() {
-                            if ui.button(&manifest.name).clicked() {
-                                self.selected_manifest = manifest_index;
-                            }
-                        }
-                    });
-                });
-
-                ui.group(|ui| {
-                    let Some((manifest_path, manifest)) =
-                        pack_manifests.get(self.selected_manifest)
-                    else {
-                        return;
-                    };
-
-                    ui.vertical(|ui| {
-                        for (variant_id, variant) in &manifest.variants {
-                            if ui.button(&variant.display_name).clicked() {
-                                match Pack::load(manifest_path, variant_id) {
-                                    Ok(loaded_pack) => {
-                                        pack = Some(loaded_pack);
-                                        ctx.request_repaint();
-                                    }
-                                    Err(err) => eprintln!("{err:#?}"),
-                                }
-                            }
-                        }
-                    });
-                });
             });
         });
 
